15018239c0164bb5d33d8fb5e7ccd72e76adb30a
Melanie Villalobos <melanievillalobos@Melanies-MacBook-Pro.local>
2022-12-20 20:41:55 -0600

    initial commit

---
+++ PokedeProject/PokedexApp/tests/test_urls.py
@@ -0,0 +1,9 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from PokedexApp.views import index
+
+class TestsPokedexAppUrls(SimpleTestCase):
+    def test_list_urls_is_resolve(self):
+        url = reverse('index')
+        self.assertEquals(resolve(url).func, index)
+        print(url)
